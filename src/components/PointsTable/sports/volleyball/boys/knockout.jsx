be09cafbe652917ef70c5933a35b7322db1a93e2
PriyanshuDev29 <142645347+PriyanshuDev29@users.noreply.github.com>
2025-11-05 14:15:52 +0530

    volleyball knockouts

--- src/components/PointsTable/sports/volleyball/boys/knockout.jsx
+++ src/components/PointsTable/sports/volleyball/boys/knockout.jsx
@@ -5,8 +5,8 @@ export const tableTennisBoysKnockout = {
         {
             name: "Semi-finals",
             matches: [
-                { id: 'SF1', date: 'TBD', venue: 'Volleyball Court', team1: 'A1', score1: '', team2: 'B2', score2: '', winner: '' },
-                { id: 'SF2', date: 'TBD', venue: 'Volleyball Court', team1: 'A2', score1: '', team2: 'B1', score2: '', winner: '' },
+                { id: 'SF1', date: 'TBD', venue: 'Volleyball Court', team1: 'ME', score1: '', team2: 'PIE+ECM', score2: '', winner: '' },
+                { id: 'SF2', date: 'TBD', venue: 'Volleyball Court', team1: 'PG', score1: '', team2: 'CSE', score2: '', winner: '' },
             ]
         },
         {
